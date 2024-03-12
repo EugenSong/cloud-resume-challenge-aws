631e52a3cf90b290987c362d1a3ca317856bf748
Eugene <75242911+EugenSong@users.noreply.github.com>
2024-03-11 22:56:53 -0700

    moto groundbreaking change in 5.0... CHANGELOG..
    all decorators are replaced w/ single mock_aws
    then use 'with mock_aws():'

--- tests/test_visitorCounter_lambda_function.py
+++ tests/test_visitorCounter_lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 from decimal import Decimal
-from moto import mock_dynamodb2
+from moto import mock_aws
 import pytest
 import sys
 import os
@@ -16,7 +16,7 @@ def dynamodb():
     Create a mock DynamoDB instance with the VisitorCounts table.
     Yields the DynamoDB resource instance.
     """
-    with mock_dynamodb2():
+    with mock_aws():
         dynamodb_resource = boto3.resource("dynamodb", region_name="us-east-1")
         table = dynamodb_resource.create_table(
             TableName="VisitorCounts",
@@ -30,11 +30,25 @@ def dynamodb():
 def test_successful_update(dynamodb):
     """Test a successful DynamoDB update"""
     table = dynamodb.Table("VisitorCounts")
-    response = lambda_handler()
-    response_body = json.loads(response["body"])
+
+    # Create a sample event object
+    event = {
+        "httpMethod": "GET",
+        "queryStringParameters": None
+    }
+
+    # Call the lambda_handler function with the event object
+    response = lambda_handler(event, None)
+
+    # Assert the response
     assert response["statusCode"] == 200
     assert "application/json" in response["headers"]["Content-Type"]
+
+    # Parse the response body
+    response_body = json.loads(response["body"])
     assert response_body["updatedCount"] == 1
     assert response_body["message"] == "Visit count updated successfully"
+
+    # Assert the updated count in the DynamoDB table
     updated_count = table.get_item(Key={"PageID": "ResumePage"})["Item"]["VisitCount"]
     assert updated_count == 1
