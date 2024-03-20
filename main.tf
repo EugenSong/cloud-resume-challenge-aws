0e9c438bfa5957d6b0551410486230db3dbcac5c
Eugene <75242911+EugenSong@users.noreply.github.com>
2024-03-19 22:52:03 -0700

    frontend workflow created

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   # s3 bucket to store tf state files
   backend "s3" {
     bucket = "eugene-terraform-bucket"
-    key    = "terraform.tfstate" 
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
