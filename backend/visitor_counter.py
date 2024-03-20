a0ffa65da85097ca8d9164f268b9b7e5d824ab81
Eugene <75242911+EugenSong@users.noreply.github.com>
2024-03-19 17:54:24 -0700

    testing2..

--- backend/visitor_counter.py
+++ backend/visitor_counter.py
@@ -2,7 +2,7 @@ import json
 import boto3
 from decimal import Decimal
 
-# Specify the profile to use (ONLY for Local testing.. added set-up Node actions.. changed relative path..need to destroy terraform from github actions..)
+# Specify the profile to use (ONLY for Local testing.. added set-up Node actions.. changed relative path..need to destroy terraform from github actions.)
 # boto3.setup_default_session(profile_name="iamadmin-general")
 
 
